c933a93acf57f358e14cd4f0204ccd84fe92516c
martin boissier <martin.boissier@gmail.com>
2011-12-28 09:37:38 +0000

    definition of the signature

---
+++ source/Ant.hs
@@ -0,0 +1,10 @@
+-- | This module implements different strategies to play the Ant wars game
+module Ant (move) where
+
+import Grid
+              
+-- | find the next move of an ant on a specific grid
+move :: Grid -> Ant -> Move
+move = undefined
+
+            
