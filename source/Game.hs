9bb0abc3249350ce44e8371d64fb4daad9fc3e07
martin boissier <martin.boissier@gmail.com>
2011-12-28 09:42:10 +0000

    signatures added to game and ant modules

---
+++ source/Game.hs
@@ -0,0 +1,20 @@
+-- | This module contains functions to run matches between different implementations of ant behaviours
+module Game (Game(..)
+            ,  runGame
+            , saveGame
+            , tournament
+            ) where
+
+import Grid
+import Ant
+
+data Game
+
+-- | run a game between two ants     
+runGame = undefined 
+
+-- | tournament between mutiple ants
+tournament = undefined
+
+-- | save a game to the filesystem
+saveGame = undefined
