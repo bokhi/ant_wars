8a52933bedb41127e276bcedcae97303e7a041a4
martin boissier <martin.boissier@gmail.com>
2011-12-30 19:20:27 +0000

    Grid module updated to make use of infinite lists

--- source/Game.hs
+++ source/Game.hs
@@ -1,13 +1,15 @@
 -- | This module contains functions to run matches between different implementations of ant behaviours
 module Game (Game(..)
             , initGame
-            ,  runGame
+            , runGame
+            , runMatch
             , saveGame
             , tournament
             ) where
 
 import System.Random
 import Data.List
+import Helper
 import Grid
 import Ant
 
@@ -21,32 +23,29 @@ data Game = Game {initialGrid :: Grid
                  } 
 
 -- | Initialise a game
-initGame :: StdGen -> [Grid -> Direction] -> Game
-initGame gen mvs = Game gr gr a
-  where gr = generateGrid gen
-        a = map (\ (mv, i) -> initAnt i mv) $ zip mvs [1..]
+initGame :: Grid -> [Grid -> Direction] -> Game
+initGame gr mvs = Game gr gr a
+  where a = map (\ (mv, i) -> initAnt i mv) $ zip mvs [1..]
 
 -- | Given a game and a specific ant, update the grid by moving the ant
 updateGame :: Game -> AntNb -> Game
-updateGame g a = 
-  Game (initialGrid g) gr' (replaceNth (pred a) ant' (ants g))
-    where
-      updateAnt a g = 
-        let direction = move a $ g
-            g' = updateGrid g (antNb a) direction  in
-        (Ant (antNb a) (direction:(directions a)) (score a + (foodLeft g - foodLeft g')) (antCollision g') (move a), g')
-      ant = ants g !! (pred a)
-      gr = grid g
-      (ant', gr') = updateAnt ant gr
+updateGame g a = Game (initialGrid g) gr' (replaceNth (pred a) ant' (ants g))
+  where
+    updateAnt a g = 
+      let direction = move a $ g
+          g' = updateGrid g (antNb a) direction  
+      in (Ant (antNb a) (direction:(directions a)) (score a + (foodLeft g - foodLeft g')) (antCollision g') (move a), g')
+    ant = ants g !! (pred a)
+    gr = grid g
+    (ant', gr') = updateAnt ant gr
       
 -- | To test whether a game is over or not      
 endGame :: Game -> Bool      
-endGame g =
-  let dead = any (\ a -> kill a)  $ ants g
-      end = all (\ a -> length (directions a) >= nbMove) (ants g)
-  in dead || end
-
-                   
+endGame g = dead || end
+  where
+    dead = any (\ a -> kill a)  $ ants g
+    end = all (\ a -> length (directions a) >= nbMove) (ants g)
+            
 -- | run a game between two ants     
 runGame :: Game -> Game
 runGame g = runGame' g 0
@@ -58,6 +57,15 @@ runGame g = runGame' g 0
 -- | winner of a game
 winnerGame :: Game -> AntNb
 winnerGame g = antNb (head $ sortBy (\ a a' -> compare (score a) (score a')) (ants g)) -- TODO verify that 1 is winning in case of tie
+
+-- | run a set of games
+runMatch :: StdGen -> [Grid -> Direction] -> (AntNb, [Game])
+runMatch gen moves = undefined
+--  game = runGame $ initGame gen moves
+
+-- | ant winner of a match
+winnerMatch :: [Game] -> AntNb
+winnerMatch = undefined
 
 -- | tournament between mutiple ants
 tournament = undefined
--- source/Grid.hs
+++ source/Grid.hs
@@ -1,90 +1,76 @@
 -- | This module contains the representation of a type Grid as well as functions to generate and modify grids
 module Grid (Grid(..)
-            , generateGrid
+            , Direction(..)
+            , AntNb
+            , generateGrids
             , updateGrid
             , rotateGrid
             , foodLeft
             , fovGrid
             , antCollision
-            , Direction(..)
-            , AntNb
-            , replaceNth -- TODO : move to a helper function module
             ) where 
 
 import System.Random
 import Data.List
 import Data.Char
+import Helper
 
 dimension = 11
 nbFood = 15
 fov = 5
 antInitialPosition = [(5,  2), (5, 8)]
 
-replaceNth n newVal (x:xs)
-     | n == 0 = newVal:xs
-     | otherwise = x:replaceNth (n-1) newVal xs
-
-
-
-
 -- | The food and ants are represented as lists of points on the grid
 data Grid = Grid { food :: [(Int, Int)]
                  , antPositions :: [(Int, Int)]
                  } 
                             
--- | This function generates a dimension*dimension grid containing nbFood pieces of food             
-generateGrid :: StdGen -> Grid                            
-generateGrid gen = Grid food antInitialPosition
-  where random = randomRs (0, (pred dimension)) gen :: [Int]
-        food = generate random []
-        generate random l = -- exactly nbFood pieces are wanted, they have to differ from each others and from the ants' positions
-          if length l == nbFood
-          then l
-          else let (x:x':xs) = random in 
-          if (x, x') `elem` l || (x, x') == (5, 2) || (x, x') == (5, 8)
-          then generate xs l
-          else generate xs ((x, x'):l)
-
-
-                   
-coord (x, y) = (succ dimension)*2*(2*x + 1) + 2*y + 1 -- translate a point to its position in the ascii table
-
-showGrid :: Grid -> String
-showGrid g = 
-  let row  = intersperse '-' (replicate (succ dimension) '+') ++ "\n"
-      column  = intersperse ' ' (replicate (succ dimension) '|') ++ "\n"
-      grid = intercalate column (replicate (succ dimension) row)
-      grid' = foldl (\ g pos -> replaceNth (coord pos) 'F' g) grid (food g)
-      (grid'', _) = foldl (\ (g, n) pos -> (replaceNth (coord pos) (intToDigit n) g, succ n)) (grid', 1) (antPositions g)
-  in grid''
-     
--- | The grid can be represented as a ASCII table, F for pieces of food, 1 and 2 for ant1 and ant2 respectively     
-instance Show Grid where
-  show g = showGrid g
-
 -- | to express the motion an ant is able to do
-data Direction = NW | N | NE | E | SE | S | SW | W deriving Eq
-
-updatePos (x, y) m = 
-  case m of 
-    NW -> (mod (x - 1) dimension, mod (y - 1) dimension)
-    N -> (mod (x - 1) dimension, y)
-    NE -> (mod (x - 1) dimension, mod (y + 1) dimension)
-    E -> (x, mod (y + 1) dimension)
-    SE -> (mod (x + 1) dimension, mod (y + 1) dimension)
-    S -> (mod (x + 1) dimension, y)
-    SW -> (mod (x + 1) dimension, mod (y - 1) dimension)
-    W -> (x, mod (y - 1) dimension)
-
-updateFood f p m = delete (updatePos p m) f
+data Direction = NW | N | NE | E | SE | S | SW | W deriving (Show, Eq)
 
 type AntNb = Int
+
+-- | Generate an infinite number of dimension*dimension grids containing nbFood pieces of food
+generateGrids :: StdGen -> [Grid]
+generateGrids gen = generateGrids' (randomRs (0, (pred dimension)) gen :: [Int])
+  where
+      generateFood random l = -- exactly nbFood pieces are wanted, they have to differ from each others and from the ants' positions
+        if length l == nbFood
+        then (l, random)
+        else let (x:x':xs) = random in 
+        if (x, x') `elem` l || (x, x') == (5, 2) || (x, x') == (5, 8)
+        then generateFood xs l
+        else generateFood xs ((x, x'):l)
+      generateGrids' random = Grid food antInitialPosition : generateGrids' random'
+        where (food, random') = generateFood random []
+
+-- | The grid can be represented as a ASCII table, F for pieces of food, 1 and 2 for ant1 and ant2 respectively     
+instance Show Grid where
+  show g =   
+    let coord (x, y) = (succ dimension)*2*(2*x + 1) + 2*y + 1 -- translate a point to its position in the ascii table
+        row  = intersperse '-' (replicate (succ dimension) '+') ++ "\n"
+        column  = intersperse ' ' (replicate (succ dimension) '|') ++ "\n"
+        grid = intercalate column (replicate (succ dimension) row)
+        grid' = foldl (\ g pos -> replaceNth (coord pos) 'F' g) grid (food g)
+        (grid'', _) = foldl (\ (g, n) pos -> (replaceNth (coord pos) (intToDigit n) g, succ n)) (grid', 1) (antPositions g)
+    in grid''
 
 -- | This function generates a new grid following the move of a specific ant
 updateGrid :: Grid -> AntNb -> Direction -> Grid
 updateGrid g n m  = 
   Grid food' (replaceNth (pred n) pos' (antPositions g))
     where
+      updatePos (x, y) m = 
+        case m of 
+          NW -> (mod (x - 1) dimension, mod (y - 1) dimension)
+          N -> (mod (x - 1) dimension, y)
+          NE -> (mod (x - 1) dimension, mod (y + 1) dimension)
+          E -> (x, mod (y + 1) dimension)
+          SE -> (mod (x + 1) dimension, mod (y + 1) dimension)
+          S -> (mod (x + 1) dimension, y)
+          SW -> (mod (x + 1) dimension, mod (y - 1) dimension)
+          W -> (x, mod (y - 1) dimension)
+      updateFood f p m = delete (updatePos p m) f
       pos' = updatePos (antPositions g !! (pred n)) m
       food' = delete pos' $ food g
 
@@ -107,8 +93,8 @@ fovGrid g n = Grid (filter f $ food g) (filter f $ antPositions g)
               x'' = abs $ x - x'
               y'' = abs $ y - y' 
               fov' = fov `div` 2
-          in
-          (min x'' $ dimension - x'') <= fov' && (min y'' $ dimension - y'') <= fov'
+          in (min x'' $ dimension - x'') <= fov' && (min y'' $ dimension - y'') <= fov'
+           
         
           
            
