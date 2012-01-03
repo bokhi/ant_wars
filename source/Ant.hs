b66176602d110bcd7be013f8038fb24780150cfd
martin boissier <martin.boissier@gmail.com>
2012-01-03 15:01:27 +0000

    Modification of Game, possibility to runMatches fully implemented and correct

--- source/Ant.hs
+++ source/Ant.hs
@@ -1,6 +1,7 @@
 -- | This module implements different strategies to play the Ant wars game
 module Ant (Ant(..)
            , initAnt
+           , updateAnt
            , testMove
            ) where
 
@@ -19,6 +20,14 @@ instance Show Ant where
 -- | Initiate an ant given a move function and an id
 initAnt :: AntNb -> (Grid -> Direction) -> Ant
 initAnt a m = Ant a [] 0 False m
+
+-- | Move an ant on a grid to update the ant and grid structure
+updateAnt :: Ant -> Grid -> (Ant, Grid)
+updateAnt a g = 
+  let direction = move a $ g
+      g' = updateGrid g (antNb a) direction  
+  in (Ant (antNb a) (direction:(directions a)) (score a + (foodLeft g - foodLeft g')) (antCollision g') (move a), g')
+
 
 -- initAnt
 
--- source/Game.hs
+++ source/Game.hs
@@ -14,8 +14,8 @@ import Grid
 import Ant
 
 nbMove = 35
-nbMatch = 3
-nbVictory = 2
+nbMatch = 5
+nbVictory = 3
 
 data Game = Game {initialGrid :: Grid
                  , grid :: Grid
@@ -32,22 +32,20 @@ initGame gr mvs = Game gr gr a
 
 -- | Given a game and a specific ant, update the grid by moving the ant
 updateGame :: Game -> AntNb -> Game
-updateGame g a = Game (initialGrid g) gr' (replaceNth (pred a) ant' (ants g))
-  where
-    updateAnt a g = 
-      let direction = move a $ g
-          g' = updateGrid g (antNb a) direction  
-      in (Ant (antNb a) (direction:(directions a)) (score a + (foodLeft g - foodLeft g')) (antCollision g') (move a), g')
-    ant = ants g !! (pred a)
-    gr = grid g
-    (ant', gr') = updateAnt ant gr
+updateGame g a = 
+  if kill $ ants g !! (a `mod` 2) -- if the ant has been killed no update
+  then g
+  else Game (initialGrid g) gr' (replaceNth (pred a) ant' (ants g))
+    where
+      ant = ants g !! (pred a)
+      gr = grid g
+      (ant', gr') = updateAnt ant gr
+    
+
       
 -- | To test whether a game is over or not      
 endGame :: Game -> Bool      
-endGame g = dead || end
-  where
-    dead = any (\ a -> kill a)  $ ants g -- Error ! a game does not end when an ant dies
-    end = all (\ a -> length (directions a) >= nbMove) (ants g)
+endGame g = any (\ a -> length (directions a) >= nbMove) (ants g)
             
 -- | Winner of a game
 winnerGame :: Game -> AntNb    
@@ -69,7 +67,7 @@ runMatch gen moves = (if score 1 >= 3 then 1 else 2, games') -- Works only for t
     games = map (\ grid -> initGame grid moves) grids 
     games' = map runGame games
     winners = map winnerGame games'
-    score ant = foldl (\ nb sum -> if nb == ant then sum + 1 else sum) 0 winners
+    score ant = foldl (\ sum nb -> if nb == ant then sum + 1 else sum) 0 winners
 
 -- | tournament between mutiple ants
 tournament = undefined
