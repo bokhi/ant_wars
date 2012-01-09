a8837c9cfeb320bb50d6e49f54d5a7a282b40b31
martin boissier <martin.boissier@gmail.com>
2012-01-09 16:22:54 +0000

    simplification of the tutorial

--- source/Genetic.hs
+++ source/Genetic.hs
@@ -14,33 +14,29 @@ import Control.Monad.Random
 data E = Plus E E
        | Minus E E
        | Times E E
-       | Div E E
        | Const Int
        deriving (Typeable,Data,Eq,Show)
                                                     
-eval :: E -> Maybe Int                
-eval (Const c)     = Just c
-eval (Plus e1 e2)  = liftM2 (+) (eval e1) (eval e2)
-eval (Minus e1 e2) = liftM2 (-) (eval e1) (eval e2)
-eval (Times e1 e2) = liftM2 (*) (eval e1) (eval e2)
-eval (Div e1 e2) | ok        = liftM2 div x1 x2
-                 | otherwise = Nothing
-                   where (x1,x2) = (eval e1,eval e2)
-                         ok = x2 /= Just 0 && liftM2 mod x1 x2 == Just 0
+eval :: E ->  Int                
+eval (Const c)     =  c
+eval (Plus e1 e2)  =  (+) (eval e1) (eval e2)
+eval (Minus e1 e2) =  (-) (eval e1) (eval e2)
+eval (Times e1 e2) =  (*) (eval e1) (eval e2)
                          
 instance GenProg (Rand StdGen) E where                         
-  terminal    =  liftM Const $ getRandomR (1, 9) -- modif
+  terminal    =  liftM Const $  getRandomR (1,9)
   nonterminal = do
-    r <- getRandomR (0,3)
-    [liftM2 Plus terminal terminal,
-     liftM2 Minus terminal terminal,
-     liftM2 Times terminal terminal,
-     liftM2 Div terminal terminal] !! r
+    r <- getRandomR (0,2)
+    [ liftM2 Plus terminal terminal,
+      liftM2 Minus terminal terminal,
+      liftM2 Times terminal terminal] !! r
       
 myFitness :: Int -> E -> Double      -- modif
 myFitness n e = error + size
-  where error = realToFrac $ maybe maxBound (abs . (n-)) (eval e)
+  where error = realToFrac  (abs  (n - (eval e)))
         size  = (realToFrac $ nodes e) / 100
                         
 
-    
+params = defaultEvolParams { fitness = myFitness 12345 }
+g = mkStdGen 0
+i = cachedBest $ evalRand (evolve params) g
