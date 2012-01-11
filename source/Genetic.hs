a15d9d453c4ffcad442bf46512a054a81f8c5008
martin boissier <martin.boissier@gmail.com>
2012-01-11 19:14:05 +0000

    Genetic module functions rewritten to use a StdGen seed

--- source/Genetic.hs
+++ source/Genetic.hs
@@ -76,24 +76,24 @@ replaceBB pos b1 b2 n = replace' pos b1 b2 n
     replace' pos b1 b2 n
       | pos == n = b2
       | otherwise = case b1 of
-        And b1' b2' -> And (replace' pos b1' b2 (succ n)) (replace' pos b2' b2 (n + 2 + nodeB b1'))
-        Or b1' b2' -> Or (replace' pos b1' b2 (succ n)) (replace' pos b2' b2 (n + 2 + nodeB b1'))
+        And b1' b2' -> And (replace' pos b1' b2 (succ n)) (replace' pos b2' b2 (succ n + nodeB b1'))
+        Or b1' b2' -> Or (replace' pos b1' b2 (succ n)) (replace' pos b2' b2 (succ n + nodeB b1'))
         Not b -> Not (replace' pos b b2 (succ n))
         IsFood x -> IsFood x
         IsEnemy x -> IsEnemy x
-        IsSmaller i1 i2 -> IsSmaller (replaceIB pos i1 b2 (succ n)) (replaceIB pos i2 b2 (n + 2 + nodeI i1))
+        IsSmaller i1 i2 -> IsSmaller (replaceIB pos i1 b2 (succ n)) (replaceIB pos i2 b2 (succ n + nodeI i1))
                 
 -- | replace i in b at position pos                
 replaceBI :: Int -> B -> I -> Int -> B  
 replaceBI pos b i n = replace' pos b i n
   where
     replace' pos b i n = case b of
-      And b1 b2 -> And (replace' pos b1 i (succ n)) (replace' pos b2 i (n + 2 + nodeB b1))
-      Or b1 b2 -> Or (replace' pos b1 i (succ n)) (replace' pos b2 i (n + 2 + nodeB b1))
+      And b1 b2 -> And (replace' pos b1 i (succ n)) (replace' pos b2 i (succ n + nodeB b1))
+      Or b1 b2 -> Or (replace' pos b1 i (succ n)) (replace' pos b2 i (succ n + nodeB b1))
       Not b' -> Not (replace' pos b' i (succ n))
       IsFood x -> IsFood x
       IsEnemy x -> IsEnemy x
-      IsSmaller i1 i2 -> IsSmaller (replaceII pos i1 i (succ n)) (replaceII pos i2 i (n + 2 + nodeI i1))
+      IsSmaller i1 i2 -> IsSmaller (replaceII pos i1 i (succ n)) (replaceII pos i2 i (succ n + nodeI i1))
   
 
 -- | replace i2 in i1 at position pos                            
@@ -104,10 +104,10 @@ replaceII pos i1 i2 n = replace' pos i1 i2 n
       | pos == n = i2
       | otherwise = case i1 of
         Const i -> Const i
-        Add i1' i2' -> Add (replace' pos i1' i2 (succ n)) (replace' pos i2' i2 (n + 2 + nodeI i1'))
-        Sub i1' i2' -> Sub (replace' pos i1' i2 (succ n)) (replace' pos i2' i2 (n + 2 + nodeI i1'))
-        Mul i1' i2' -> Mul (replace' pos i1' i2 (succ n)) (replace' pos i2' i2 (n + 2 + nodeI i1'))
-        If b i1' i2' -> If (replaceBI pos b i2 (succ n)) (replace' pos i1' i2 (n + 2 + nodeB b)) (replace' pos i2' i2 (n + 3 + nodeB b + nodeI i1))
+        Add i1' i2' -> Add (replace' pos i1' i2 (succ n)) (replace' pos i2' i2 (succ n + nodeI i1'))
+        Sub i1' i2' -> Sub (replace' pos i1' i2 (succ n)) (replace' pos i2' i2 (succ n + nodeI i1'))
+        Mul i1' i2' -> Mul (replace' pos i1' i2 (succ n)) (replace' pos i2' i2 (succ n + nodeI i1'))
+        If b i1' i2' -> If (replaceBI pos b i2 (succ n)) (replace' pos i1' i2 (succ n + nodeB b)) (replace' pos i2' i2 (succ n + nodeB b + nodeI i1))
         
 -- | replace b in i at position pos        
 replaceIB :: Int -> I -> B -> Int -> I
@@ -115,10 +115,10 @@ replaceIB pos i b n = replace' pos i b n
   where
     replace' pos i b n = case i of
       Const i -> Const i
-      Add i1 i2 -> Add (replace' pos i1 b (succ n)) (replace' pos i2 b (n + 2 + nodeI i1))
-      Sub i1 i2 -> Sub (replace' pos i1 b (succ n)) (replace' pos i2 b (n + 2 + nodeI i1))
-      Mul i1 i2 -> Mul (replace' pos i1 b (succ n)) (replace' pos i2 b (n + 2 + nodeI i1))
-      If b' i1 i2 -> If (replaceBB pos b' b (succ n)) (replace' pos i1 b (n + 2 + nodeB b')) (replace' pos i2 b (n + 3 + nodeB b' + nodeI i1))
+      Add i1 i2 -> Add (replace' pos i1 b (succ n)) (replace' pos i2 b (succ n + nodeI i1))
+      Sub i1 i2 -> Sub (replace' pos i1 b (succ n)) (replace' pos i2 b (succ n + nodeI i1))
+      Mul i1 i2 -> Mul (replace' pos i1 b (succ n)) (replace' pos i2 b (succ n + nodeI i1))
+      If b' i1 i2 -> If (replaceBB pos b' b (succ n)) (replace' pos i1 b (succ n + nodeB b')) (replace' pos i2 b (succ n + nodeB b' + nodeI i1))
                            
 -- | evaluate a B expression                           
 evalB :: B -> AntNb -> Memory -> Grid -> Bool
@@ -143,42 +143,43 @@ evalI (If b f1 f2) a m g = if evalB b a m g then evalI f1 a m g else evalI f2 a 
 rect2List :: Rect -> [(Int, Int)]
 rect2List (x, y, dx, dy) = [(x' `mod` dimension , y' `mod` dimension) | x' <- [x..(x+dx)], y' <- [y..(y+dy)]]
 
--- | given an (infinite) list of pseudo-generated integer, construct a Rect
-generateA :: [Int] -> Int -> Rect
-generateA (a:b:c:d:xs) _ = (a `mod` dimension, b `mod` dimension, c `mod` dx, d `mod` dy)
 
--- | given an (infinite) list of pseudo-generated integer, construct a B expression of height h
-generateB :: [Int] -> Int -> B
-generateB (x:xs) h = 
-  if h <= 2 
-  then if x `mod` 2 == 0 then IsFood (generateA xs h) else IsEnemy (generateA xs h)
-  else 
-    case x `mod` 6 of
-      0 -> And (generateB xs $ pred h) (generateB xs' $ pred h)
-      1 -> Or (generateB xs $ pred h) (generateB xs' $ pred h)
-      2 -> Not (generateB xs $ pred h)
-      3 -> IsFood (generateA xs $ pred h)
-      4 -> IsEnemy (generateA xs $ pred h)
-      5 -> IsSmaller (generateI xs $ pred h) (generateI xs' $ pred h) 
-      where xs' = tail xs
+-- | 
+generateB :: StdGen -> Int -> B            
+generateB gen d 
+  | d <= 2 = if fst (random gen ) :: Bool then IsFood rect else IsEnemy rect
+  | otherwise = case fst (randomR (0, 5) gen) :: Int of 
+    0 -> And (generateB g $ pred d) (generateB g' $ pred d)
+    1 -> Or (generateB g $ pred d) (generateB g' $ pred d)
+    2 -> Not (generateB g $ pred d)
+    3 -> IsFood rect
+    4 -> IsEnemy rect
+    5 -> IsSmaller (generateI g $ pred d) (generateI g' $ pred d) 
+    where 
+      (a:b:c:d:_) = randoms g
+      rect = (a `mod` dimension, b `mod` dimension, c `mod` dx, d `mod` dy)
+      (g, g') = split gen
 
--- | given an (infinite) list of pseudo-generated integer, construct a I expression of height h
-generateI :: [Int] -> Int -> I
-generateI (x:xs) h = 
-  if h <= 1 
-  then Const x
-  else
-    case x `mod` 4 of
-      0 -> Add (generateI xs $ pred h) (generateI xs' $ pred h)
-      1 -> Const x
-      2 -> Sub (generateI xs $ pred h) (generateI xs' $ pred h)
-      _ -> If (generateB xs $ pred h) (generateI xs' $ pred h) (generateI xs'' $ pred h)
-      where xs' = tail xs
-            xs'' = tail xs'
+-- | given a seed, construct a I expression of height h
+generateI :: StdGen -> Int -> I
+generateI gen d 
+  | d <= 2 = Const x 
+  | otherwise = case fst (randomR (0, 3) gen) :: Int of
+    0 -> Add (generateI g  $ pred d) (generateI g' $ pred d)
+    1 -> Const x
+    2 -> Sub (generateI  g $ pred d) (generateI g' $ pred d)
+    3 -> If (generateB g $ pred d) (generateI g' $ pred d) (generateI g'' $ pred d)
+    where
+      x = fst $ randomR (0, 5) gen :: Int
+      (g, gen') = split gen
+      (g', g'') = split gen'
+
 
 gen = mkStdGen 0
-grids = generateGrids gen
-xs = randomRs (0, 10) (mkStdGen 0) :: [Int]
+(g, g') = split gen
+grids = generateGrids g'
+
+i = generateI g 3
 
 
 
